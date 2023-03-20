0e2f3ed8af127a920cedbbd4831522299d4dd118
davitdarbaidze <daviddarbaidze@gmail.com>
2023-03-20 18:17:00 +0100

    carousel v2

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,34 +1,56 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/carousel.module.scss';
 
 const MOVIES = [
-  { id: 1, title: 'Movie 1', imageUrl: 'http://placeimg.com/640/480/animals' },
-  { id: 2, title: 'Movie 2', imageUrl: 'http://placeimg.com/640/480/animals' },
-  { id: 3, title: 'Movie 3', imageUrl: 'http://placeimg.com/640/480/animals' },
-  { id: 4, title: 'Movie 4', imageUrl: 'http://placeimg.com/640/480/animals' },
-  { id: 5, title: 'Movie 5', imageUrl: 'http://placeimg.com/640/480/animals' },
-  { id: 5, title: 'Movie 5', imageUrl: 'http://placeimg.com/640/480/animals' },
-  { id: 5, title: 'Movie 5', imageUrl: 'http://placeimg.com/640/480/animals' },
+  { id: 1, title: 'Movie 1', imageUrl: 'https://placeimg.com/640/480/animals?v=1' },
+  { id: 1, title: 'Movie 1', imageUrl: 'https://placeimg.com/640/480/animals?v=2' },
+  { id: 1, title: 'Movie 1', imageUrl: 'https://placeimg.com/640/480/animals?v=3' },
+  { id: 1, title: 'Movie 1', imageUrl: 'https://placeimg.com/640/480/animals?v=4' },
+  { id: 1, title: 'Movie 1', imageUrl: 'https://placeimg.com/640/480/animals?v=5' },
+  { id: 1, title: 'Movie 1', imageUrl: 'https://placeimg.com/640/480/animals?v=6' },
+  { id: 2, title: 'Movie 2', imageUrl: 'https://placeimg.com/640/480/animals?v=7' },
+  { id: 2, title: 'Movie 2', imageUrl: 'https://placeimg.com/640/480/animals?v=8' },
+  { id: 2, title: 'Movie 2', imageUrl: 'https://placeimg.com/640/480/animals?v=9' },
+  { id: 2, title: 'Movie 2', imageUrl: 'https://placeimg.com/640/480/animals?v=10' },
+  { id: 2, title: 'Movie 2', imageUrl: 'https://placeimg.com/640/480/animals?v=11' },
+  { id: 2, title: 'Movie 2', imageUrl: 'https://placeimg.com/640/480/animals?v=12' },
+  { id: 2, title: 'Movie 2', imageUrl: 'https://placeimg.com/640/480/animals?v=13' },
+  { id: 3, title: 'Movie 2', imageUrl: 'https://placeimg.com/640/480/animals?v=14' },
+  { id: 3, title: 'Movie 4', imageUrl: 'https://placeimg.com/640/480/animals?v=15' },
+  { id: 3, title: 'Movie 4', imageUrl: 'https://placeimg.com/640/480/animals?v=16' },
+  { id: 3, title: 'Movie 4', imageUrl: 'https://placeimg.com/640/480/animals?v=17' },
+  { id: 3, title: 'Movie 4', imageUrl: 'https://placeimg.com/640/480/animals?v=18' },
+  { id: 3, title: 'Movie 4', imageUrl: 'https://placeimg.com/640/480/animals?v=19' },
+  { id: 4, title: 'Movie 4', imageUrl: 'https://placeimg.com/640/480/animals?v=20' },
+  { id: 4, title: 'Movie 4', imageUrl: 'https://placeimg.com/640/480/animals?v=21' },
+  { id: 4, title: 'Movie 4', imageUrl: 'https://placeimg.com/640/480/animals?v=22' },
+  { id: 4, title: 'Movie 5', imageUrl: 'https://placeimg.com/640/480/animals?v=23' },
+  { id: 4, title: 'Movie 5', imageUrl: 'https://placeimg.com/640/480/animals?v=24' },
 ];
 
-const MoviesCarousel = () => {
+const MoviesCarousel = ({ movies }) => {
+  const [currentPage, setCurrentPage] = useState(1);
   const [activeIndex, setActiveIndex] = useState(0);
-  const containerRef = useRef(null);
+  const itemsPerPage = 6;
+  const totalPages = Math.ceil(MOVIES.length / itemsPerPage);
 
-  const handlePrevClick = () => {
-    setActiveIndex(activeIndex === 0 ? MOVIES.length - 1 : activeIndex - 1);
+  const handleClickPrev = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
   };
 
-  const handleNextClick = () => {
-    setActiveIndex(activeIndex === MOVIES.length - 1 ? 0 : activeIndex + 1);
+  const handleClickNext = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
   };
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
 
   return (
     <div className={styles.movies_carousel}>
-      <div className={styles.movies_carousel__container} ref={containerRef}>
-        {MOVIES.map((movie, index) => (
+      <div className={styles.movies_carousel__container}>
+        {MOVIES.slice(startIndex, endIndex).map((movie, index) => (
           <div
-            key={movie.id}
+            key={index}
             className={`${styles.movies_carousel__movie} ${
               index === activeIndex ? `${styles.active}` : ''
             }`}
@@ -38,15 +60,21 @@ const MoviesCarousel = () => {
           </div>
         ))}
       </div>
-      <button className={styles.movies_carousel__prev} onClick={handlePrevClick}>
-        &lt;
+      <button
+        className={styles.movies_carousel__prev} 
+        onClick={handleClickPrev}
+        disabled={currentPage === 1}
+      >
+        {'<'}
       </button>
-      <button className={styles.movies_carousel__next} onClick={handleNextClick}>
-        &gt;
+      <button
+        className={styles.movies_carousel__next}
+        onClick={handleClickNext}
+        disabled={currentPage === totalPages}
+      >
+        {'>'}
       </button>
-      
     </div>
-    
   );
 };
 
--- src/pages/test.js
+++ src/pages/test.js
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, {useState, useRef} from "react";
+import styles from "../styles/carousel.module.scss";
 
-export default function Test() {
+const MOVIES = [
+  { id: 1, title: 'Movie 1', imageUrl: 'https://placeimg.com/640/480/animals?v=22' },
+  { id: 2, title: 'Movie 2', imageUrl: 'https://placeimg.com/640/480/animals?v=23' },
+  { id: 3, title: 'Movie 3', imageUrl: 'https://placeimg.com/640/480/animals?v=24' },
+  { id: 4, title: 'Movie 4', imageUrl: 'https://placeimg.com/640/480/animals?v=25' },
+  { id: 5, title: 'Movie 5', imageUrl: 'https://placeimg.com/640/480/animals?v=26' },
+  { id: 5, title: 'Movie 5', imageUrl: 'https://placeimg.com/640/480/animals?v=27' },
+  { id: 5, title: 'Movie 5', imageUrl: 'https://placeimg.com/640/480/animals?v=28' },
+];
+
+const MoviesCarousel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const containerRef = useRef(null);
+
+  const handlePrevClick = () => {
+    setActiveIndex(activeIndex === 0 ? MOVIES.length - 1 : activeIndex - 1);
+  };
+
+  const handleNextClick = () => {
+    setActiveIndex(activeIndex === MOVIES.length - 1 ? 0 : activeIndex + 1);
+  };
+
   return (
-    <div>Test</div>
-  )
-}
+    <div className={styles.movies_carousel}>
+      <div className={styles.movies_carousel__container} ref={containerRef}>
+        {MOVIES.map((movie, index) => (
+          <div
+            key={movie.id}
+            className={`${styles.movies_carousel__movie} ${
+              index === activeIndex ? `${styles.active}` : ''
+            }`}
+            style={{ backgroundImage: `url(${movie.imageUrl})` }}
+          >
+            <div className={styles.movies_carousel__movie_title}>{movie.title}</div>
+          </div>
+        ))}
+      </div>
+      <button className={styles.movies_carousel__prev} onClick={handlePrevClick}>
+        &lt;
+      </button>
+      <button className={styles.movies_carousel__next} onClick={handleNextClick}>
+        &gt;
+      </button>
+      
+    </div>
+    
+  );
+};
+
+export default MoviesCarousel;

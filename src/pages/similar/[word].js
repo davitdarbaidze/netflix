2dfd4ce9d2d85126c3be31aaf007c613abfc14d3
davitdarbaidze <daviddarbaidze@gmail.com>
2023-07-27 14:00:19 +0200

    add some new features to the similar movie page

--- src/pages/similar/[word].js
+++ src/pages/similar/[word].js
@@ -3,11 +3,15 @@ import { useRouter } from "next/router";
 import Loading from "@/components/loading";
 import styles from "../../styles/moreSimilarMovies.module.scss";
 import { fetchDataFromPexels } from "@/lib/generalFunctions";
+import Router from "next/router";
+import Image from "next/image";
+import FrequentQuestions from "@/components/frequentQuestions";
 
 export default function MoreSimilarMovies(props) {
   const [display, setDisplay] = useState(false);
   const [data, setData] = useState([]);
   const { asPath } = useRouter();
+  const router = useRouter();
 
   useEffect(() => {
     if (data.length > 0) {
@@ -23,19 +27,57 @@ export default function MoreSimilarMovies(props) {
     fetchData();
   }, []);
 
+  const handleDirectToMoviePage = (e) => {
+    const videoData = JSON.parse(sessionStorage.getItem("playback_data"));
+
+    if (videoData) {
+      delete videoData.link;
+    }
+
+    const newVideoData = {
+      ...videoData,
+      link: e.target.dataset.link,
+    };
+
+    sessionStorage.setItem("playback_data", JSON.stringify(newVideoData));
+    router.push("/play");
+  };
+
   return (
     <div>
       {display ? (
         <div className={styles.similarMovieContainer}>
-        <h1>Similar Content</h1>
+          <h1>Similar Content</h1>
           {data.map((item, index) => {
             return (
               <div className={styles.similarMovieItem} key={index}>
                 <img src={item.image} alt="video Thumbnail"></img>
                 <p>{item.user.name}</p>
+                <div
+                  className={styles.playButton}
+                  onTouchEnd={handleDirectToMoviePage}
+                >
+                  <Image
+                    src="/play-outline.svg"
+                    alt="Play movie button"
+                    width={20}
+                    height={20}
+                  ></Image>
+                  <button data-link={item.video_files.link}>Play</button>
+                </div>
               </div>
             );
           })}
+          <div onTouchEnd={() => { router.push("/") }} className={styles.backButton}>            
+            <Image
+              src="/arrowBack.svg"
+              alt="Return home button"
+              width={20}
+              height={20}
+            ></Image>
+            <button>Back</button>
+          </div>
+          <FrequentQuestions/>
         </div>
       ) : (
         <div>
